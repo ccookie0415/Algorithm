c9f566c16f8774166ee6d2826f0644c57d349748
ccookie0415 <ccookie0415@gmail.com>
2023-02-02 17:29:08 +0900

    20230202

--- 20230202/flatten.py
+++ 20230202/flatten.py
@@ -6,8 +6,16 @@ T = 10
 for tc in range(T):
     N = int(input())
     box_lst = list(map(int, input().split()))
-    box_cnt = [0] * (N)
 
-    for i in range(0, len(box_lst)):
-        box_cnt[box_lst[i]] += 1
-print(box_cnt)
+    while N:
+        max_box = max(box_lst)
+        min_box = min(box_lst)
+        max_idx = box_lst.index(max(box_lst))
+        min_idx = box_lst.index(min(box_lst))
+        box_lst[max_idx] -= 1
+        box_lst[min_idx] += 1
+
+        N -= 1
+        answer = max(box_lst) - min(box_lst)
+
+    print(f'#{tc+1} {answer}')
